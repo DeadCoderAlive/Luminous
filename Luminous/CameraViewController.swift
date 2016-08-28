bbb6284e1b9f4fd4b45dc344dddeff1734214a8d
srinivaz <br.ragul@gmail.com>
2016-08-28 14:23:22 -0400

    init commit

---
+++ Luminous/CameraViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.swift
+//  Luminous
+//
+//  Created by Mac on 8/28/16.
+//  Copyright © 2016 Srinivasan. All rights reserved.
+//
+
+import Cocoa
+
+class CameraViewController: UIViewController {
+
+}
